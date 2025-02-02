a6e653d7183a8cf71db94a06b0b01dbd9992bb1e
siwon-park <zow777@naver.com>
2025-02-02 23:10:14 +0900

    feat: Project CRUD 기능 개발 중

--- src/main/java/com/dope/poiapp/repository/ProjectRepository.java
+++ src/main/java/com/dope/poiapp/repository/ProjectRepository.java
@@ -4,7 +4,13 @@ import com.dope.poiapp.domain.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-
+    List<Project> findByProjectManager(String pmName);
+    List<Project> findByCompanyName(String companyName);
+    List<Project> findByStartDateGreaterThanEqualAndEndDateLessThanEqual(Date startDate, Date endDate);
 }
--- src/main/java/com/dope/poiapp/service/ProjectService.java
+++ src/main/java/com/dope/poiapp/service/ProjectService.java
@@ -1,8 +1,12 @@
 package com.dope.poiapp.service;
 
+import com.dope.poiapp.domain.entity.Project;
 import com.dope.poiapp.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +14,27 @@ public class ProjectService {
 
     private final ProjectRepository projectRepository;
 
+    public Project getProject(long id) {
+        return projectRepository.findById(id).orElse(null);
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public List<Project> getPMProjects(String pmName) {
+        return projectRepository.findByProjectManager(pmName);
+    }
+
+    public List<Project> getCompanyProjects(String companyName) {
+        return projectRepository.findByCompanyName(companyName);
+    }
+
+    public List<Project> getBetweenDate(Date startDate, Date endDate) {
+        List<Project> projects = projectRepository.findByStartDateGreaterThanEqualAndEndDateLessThanEqual(startDate, endDate);
+        for (Project project : projects) {
+            System.out.println(project.toString());
+        }
+        return projects;
+    }
 }
